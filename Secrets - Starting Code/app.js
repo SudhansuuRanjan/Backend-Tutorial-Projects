00f22c3490825dc3c26388d95c8a1407260f6d75
SudhanshuuRanjan <sudhanshur.ug20.ee@nitp.ac.in>
2022-02-13 00:24:59 +0530

    finished project

--- Secrets - Starting Code/app.js
+++ Secrets - Starting Code/app.js
@@ -1,30 +1,29 @@
 //jshint esversion:6
-require('dotenv').config()
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-const session = require('express-session')
-const passport = require('passport');
+const session = require('express-session');
+const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
-// const bcrypt = require("bcrypt");
-// const saltRounds = 12;
-const secret = process.env.SECRET;
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require('mongoose-findorcreate');
 
 const app = express();
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({
-    extended: true
-}))
+  extended: true
+}));
 
 app.set('trust proxy', 1) // trust first proxy
 app.use(session({
-  secret: secret,
+  secret: "Our little secret.",
   resave: false,
-  saveUninitialized: true,
-}))
+  saveUninitialized: false
+}));
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -33,19 +32,42 @@ mongoose.connect("mongodb://localhost:27017/userDB",()=>{
     console.log("Databse connected!!");
 });
 
-const userSchema = new mongoose.Schema({
-    email: String,
-    password: String
+const userSchema = new mongoose.Schema ({
+  email: String,
+  password: String,
+  googleId: String,
+  secret: String
 });
 
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 
 const User = new mongoose.model("User", userSchema);
 
 passport.use(User.createStrategy());
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser((user,done)=>{
+    done(null, user.id);
+});
+passport.deserializeUser((id,done)=>{
+    User.findById(id, (err,user)=>{
+        done(err,user);
+    })
+});
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    userProfileURL:"https://www.googleapis.com/oauth2/v3/userinfo"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+      console.log(profile);
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
 
 app.get("/",(req,res)=>{
     res.render("home");
@@ -60,11 +82,15 @@ app.get("/login",(req,res)=>{
 })
 
 app.get("/secrets",(req,res)=>{
-    if(req.isAuthenticated()){
-       res.render("secrets"); 
-    }else{
-        res.redirect("/login");
-    }
+    User.find({"secret":{$ne: null}}, (err,foundUsers)=>{
+        if(err){
+           console.log(err); 
+        }else{
+            if(foundUsers){
+                res.render("secrets",{usersWithSecrets: foundUsers})
+            }
+        }
+    })
 });
 
 app.post("/register",(req,res)=>{
@@ -99,6 +125,42 @@ app.post("/login",(req,res)=>{
     })
 })
 
+app.get('/auth/google',
+    passport.authenticate('google', { scope: ["profile"] })
+);
+
+app.get('/auth/google/secrets', 
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect("/secrets");
+  });
+
+app.get('/submit',(req,res)=>{
+   if(req.isAuthenticated()){
+       res.render("submit");
+   }else{
+       res.redirect("/login");
+   } 
+});
+
+app.post('/submit',(req,res)=>{
+    const submittedSecret = req.body.secret;
+    console.log(req.user);
+
+    User.findById(req.user.id, (err, foundUser)=>{
+        if(err){
+            console.log(err);
+        }else{
+            if(foundUser){
+                foundUser.secret = submittedSecret;
+                foundUser.save(()=>{
+                    res.redirect("/secrets");
+                })
+            }
+        }
+    })
+})
 app.get("/logout",(req,res)=>{
     req.logOut();
     res.redirect("/");
